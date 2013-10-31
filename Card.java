67df77b64641cd7d8fb52d6a1c4c7c660306c7f3
akrish44 <akrish44@aol.com>
2013-10-31 09:36:51 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,83 +1,93 @@
 public class Card {
         
-        private int number;
+        private String value;
         private String suit;
+        private int value2;
 
 
-        public Card(String suit, int number) {
+        public Card(String suit, int value) {
+            this.value = value;
+            this.suit. suit;
+        }
+        public Card(String suit, int value, int value2) {
+            this.suit = suit;
+            this.value = value;
+            this.value2 = value2;
+        }
 
-         Card spades2 = new Card("Spades", 2);
-         Card hearts2 = new Card("Hearts", 2);
-         Card diamonds2 = new Card("Diamonds", 2);
-         Card clubs2 = new Card("Clubs", 2);
+
+         // Card spades2 = new Card("Spades", 2);
+         // Card hearts2 = new Card("Hearts", 2);
+         // Card diamonds2 = new Card("Diamonds", 2);
+         // Card clubs2 = new Card("Clubs", 2);
          
-         Card spades3 = new Card("Spades", 3);
-         Card hearts3 = new Card("Hearts", 3);
-         Card diamonds3 = new Card("Diamonds", 3);
-         Card clubs3 = new Card("Clubs", 3);
+         // Card spades3 = new Card("Spades", 3);
+         // Card hearts3 = new Card("Hearts", 3);
+         // Card diamonds3 = new Card("Diamonds", 3);
+         // Card clubs3 = new Card("Clubs", 3);
 
-         Card spades4 = new Card("Spades", 4);
-         Card hearts4 = new Card("Hearts", 4);
-         Card diamonds4 = new Card("Diamonds", 4);
-         Card clubs4 = new Card("Clubs", 4);
+         // Card spades4 = new Card("Spades", 4);
+         // Card hearts4 = new Card("Hearts", 4);
+         // Card diamonds4 = new Card("Diamonds", 4);
+         // Card clubs4 = new Card("Clubs", 4);
 
-         Card spades5 = new Card("Spades", 5);
-         Card hearts5 = new Card("Hearts", 5);
-         Card diamonds5 = new Card("Diamonds", 5);
-         Card clubs5 = new Card("Clubs", 5);
+         // Card spades5 = new Card("Spades", 5);
+         // Card hearts5 = new Card("Hearts", 5);
+         // Card diamonds5 = new Card("Diamonds", 5);
+         // Card clubs5 = new Card("Clubs", 5);
 
-         Card spades6 = new Card("Spades", 6);
-         Card hearts6 = new Card("Hearts", 6);
-         Card diamonds6 = new Card("Diamonds", 6);
-         Card clubs6 = new Card("Clubs", 6);
+         // Card spades6 = new Card("Spades", 6);
+         // Card hearts6 = new Card("Hearts", 6);
+         // Card diamonds6 = new Card("Diamonds", 6);
+         // Card clubs6 = new Card("Clubs", 6);
 
-         Card spades7 = new Card("Spades", 7);
-         Card hearts7 = new Card("Hearts", 7);
-         Card diamonds7 = new Card("Diamonds", 7);
-         Card clubs7 = new Card("Clubs", 7);
+         // Card spades7 = new Card("Spades", 7);
+         // Card hearts7 = new Card("Hearts", 7);
+         // Card diamonds7 = new Card("Diamonds", 7);
+         // Card clubs7 = new Card("Clubs", 7);
 
-         Card spades8 = new Card("Spades", 8);
-         Card hearts8 = new Card("Hearts", 8);
-         Card diamonds8 = new Card("Diamonds", 8);
-         Card clubs8 = new Card("Clubs", 8);
+         // Card spades8 = new Card("Spades", 8);
+         // Card hearts8 = new Card("Hearts", 8);
+         // Card diamonds8 = new Card("Diamonds", 8);
+         // Card clubs8 = new Card("Clubs", 8);
 
-         Card spades9 = new Card("Spades", 9);
-         Card hearts9 = new Card("Hearts", 9);
-         Card diamonds9 = new Card("Diamonds", 9);
-         Card clubs9 = new Card("Clubs", 9);
+         // Card spades9 = new Card("Spades", 9);
+         // Card hearts9 = new Card("Hearts", 9);
+         // Card diamonds9 = new Card("Diamonds", 9);
+         // Card clubs9 = new Card("Clubs", 9);
 
-         Card spades10 = new Card("Spades", 10);
-         Card hearts10 = new Card("Hearts", 10);
-         Card diamonds10 = new Card("Diamonds", 10);
-         Card clubs10 = new Card("Clubs", 10);
+         // Card spades10 = new Card("Spades", 10);
+         // Card hearts10 = new Card("Hearts", 10);
+         // Card diamonds10 = new Card("Diamonds", 10);
+         // Card clubs10 = new Card("Clubs", 10);
 
-         Card spadesJack = new Card("Spades", 10);
-         Card heartsJack = new Card("Hearts", 10);
-         Card diamondsJack = new Card("Diamonds", 10);
-         Card clubsJack = new Card("Clubs", 10);
+         // Card spadesJack = new Card("Spades", 10);
+         // Card heartsJack = new Card("Hearts", 10);
+         // Card diamondsJack = new Card("Diamonds", 10);
+         // Card clubsJack = new Card("Clubs", 10);
 
-         Card spadesQueen = new Card("Spades", 10);
-         Card heartsQueen = new Card("Hearts", 10);
-         Card diamondsQueen = new Card("Diamonds", 10);
-         Card clubsQueen = new Card("Clubs", 10);
+         // Card spadesQueen = new Card("Spades", 10);
+         // Card heartsQueen = new Card("Hearts", 10);
+         // Card diamondsQueen = new Card("Diamonds", 10);
+         // Card clubsQueen = new Card("Clubs", 10);
 
-         Card spadesKing = new Card("Spades", 10);
-         Card heartsKing = new Card("Hearts", 10);
-         Card diamondsKing = new Card("Diamonds", 10);
-         Card clubsKing = new Card("Clubs", 10);
+         // Card spadesKing = new Card("Spades", 10);
+         // Card heartsKing = new Card("Hearts", 10);
+         // Card diamondsKing = new Card("Diamonds", 10);
+         // Card clubsKing = new Card("Clubs", 10);
 
-         Card spadesAce = new Card("Spades", 11);
-         Card heartsAce = new Card("Hearts", 11);
-         Card diamondsAce = new Card("Diamonds", 11);
-         Card clubsAce = new Card("Clubs", 1);
+         // Card spadesAce = new Card("Spades", 11);
+         // Card heartsAce = new Card("Hearts", 11);
+         // Card diamondsAce = new Card("Diamonds", 11);
+         // Card clubsAce = new Card("Clubs", 1);
      }
 
-     public void printCards(String suits, int number) {
-     	for(int i=0; i<36; i++) {
-       		System.out.println(number + "of" + suits);
+//      public void printCards(String suits, int number) {
+//              for(int i=0; i<36; i++) {
+//                        System.out.println(number + "of" + suits);
 
-       	}
-     }
+//                }
+//      }
     
 
-}
+// }
