ec08a84a8042a25dbd854d9d89575c0d6d3e6a0a
akrish44 <akrish44@aol.com>
2013-11-07 09:32:05 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,18 +1,49 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Card {
         
-        private String value;
-        private String suit;
-        private int value2;
+        private String name;
+        private int value;
+        private Image image;
 
 
-        public Card(String suit, String value) {
+        public Card(String name, int value) {
+            this.name = name;
             this.value = value;
-            this.suit = suit;
+            this.image = Deck.loadImage(name);
         }
-        public Card(String suit, String value, int value2) {
-            this.suit = suit;
-            this.value = value;
-            this.value2 = value2;
+
+        public String name() {
+            return this.name;
         }
         
+        public void draw(Graphics g, Rectangle r) {
+            g.drawImage(image, r.x, r.y, r.width, r.height, null);
+        }
+
+    private static Image loadImage(String name) {
+        String path = null;
+        Image image = null;
+
+        try { //where you put code that could fail
+            path = "images" + File.separator + name + ".jpg";
+            //images/JurassicPark.jpg
+            image = ImageIO.read(new File(path));
+
+        } catch(IOException e) {
+            System.out.println("Could not load image at path: " + path);
+            System.exit(1);
+
+        }
+
+        return image;
+    }
+    
 }
