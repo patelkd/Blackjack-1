4370c686aad04ced4466ee909ec083858235b9d9
akrish44 <akrish44@aol.com>
2013-11-23 21:26:04 -0700

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,6 @@
+public class Human extends Player {
+
+        public Human(String name, Card initialCard, int maxScore, boolean hasButtons) {
+                super(name, initialCard, maxScore, hasButtons);
+        }
+}
